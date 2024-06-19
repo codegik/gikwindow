49795e55c257061bd3909b52eaef5193c3dcf203
Inacio Klassmann <inacioklassmann@gmail.com>
2024-06-19 13:22:58 -0300

    adding gikwindow hammespoon plugin

--- init.lua
+++ init.lua
@@ -1,7 +1,11 @@
 local windowFilter = hs.window.filter.new()
+local spaceFilter = hs.window.filter.new()
 
 windowFilter:setDefaultFilter({})
 windowFilter:setSortOrder(hs.window.filter.sortByFocusedLast)
+spaceFilter:setDefaultFilter({})
+spaceFilter:setCurrentSpace(true)
+spaceFilter:setSortOrder(hs.window.filter.sortByCreated)
 
 local function currentWindows()
 	local windows = {}
@@ -11,6 +15,30 @@ local function currentWindows()
 	end
 
 	return windows
+end
+
+local function focusNextOnThisSpace(direction)
+  local currWindow = hs.window.focusedWindow()
+  local windows = spaceFilter:getWindows()
+
+	for i, window in ipairs(windows) do
+		if window:isVisible() then
+      if window:id() == currWindow:id() then
+        local selected = i + direction
+
+        if selected <= 0 then
+          selected = #windows
+        elseif selected > #windows then
+          selected = 1
+        end
+
+        windows[selected]:focus()
+        return true
+      end
+		end
+	end
+
+  return false
 end
 
 local function focusByApp(appName)
@@ -28,4 +56,12 @@ end
 
 hs.hotkey.bind({ "alt" }, "z", function()
 	focusByApp("zoom")
+end)
+
+hs.hotkey.bind({ "alt" }, "tab", function()
+	focusNextOnThisSpace(1)
+end)
+
+hs.hotkey.bind({ "alt", "shift" }, "tab", function()
+	focusNextOnThisSpace(-1)
 end)
